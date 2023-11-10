fc997d9d69ce0dcf23818d223cd0ae6bf2832f27
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-10 23:45:06 +0300

    Added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
