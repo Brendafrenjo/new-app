4190c1d858c738ff592129524c2f19629e74c240
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-14 00:28:34 +0300

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -27,10 +27,6 @@ export default function NavBar() {
 
   useEffect(() => {
     window.addEventListener("resize", handleSize);
-
-    return () => {
-      window.addEventListener("resize", handleSize);
-    };
   }, []);
 
   return (
