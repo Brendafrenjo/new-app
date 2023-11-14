39bdedb2a954a8cef4e97c6268d55c0a43225cd1
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-15 01:56:51 +0300

    Created Home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+      <div className="Home">
+        <div className="hero">
+          <h1>Elegant. Stylish. Classy</h1>
+        </div>
+      </div>
+    );
+}
