d37fdb057cba0b1265382de9844587f3d4a33fd8
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-19 19:23:45 +0300

    Updated Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Home.css";
+import Products from "./Products"
 
 export default function Home() {
-    return (
-      <div className="Home">
-        <div className="hero">
-          <h1>Elegant. Stylish. Classy</h1>
+  return (
+    <div className="Home">
+      <div className="hero">
+        <div class="card">
+          <img
+            class="card-img-top img-fluid"
+            src="/Images/background-image.jpg"
+            alt="Background"
+          />
+          <div class="card-img-overlay">
+            <h1 class="card-title">Classy. Stylish. Fabulous</h1>
+            <p class="card-text">
+              <a href="/" class="btn btn-primary">
+                Shop Now!
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    );
+      <Products />
+    </div>
+  );
 }
