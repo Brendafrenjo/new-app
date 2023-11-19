116560aace6c0841e55c8618926072c25d38d17d
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-19 21:43:39 +0300

    Added Buttons

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 export default function Products() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="Products mb-5">
+      <h1 className="mt-5 mb-3">Latest Products</h1>
+      <hr />
+      <a href="/" class="btn btn-outline-dark me-2 mt-2">
+        All
+      </a>
+      <a href="/" class="btn btn-outline-dark me-2 mt-2">
+        Men's Clothing
+      </a>
+      <a href="/" class="btn btn-outline-dark me-2 mt-2">
+        Women's Clothing
+      </a>
+      <a href="/" class="btn btn-outline-dark me-2 mt-2">
+        Jewelery
+      </a>
+      <a href="/" class="btn btn-outline-dark mt-2">
+        Electronics
+      </a>
+    </div>
+  );
 }
