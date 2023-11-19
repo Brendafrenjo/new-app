e4085a9b4cc288eb7d5d2aa7fc8196e739682292
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-20 00:12:31 +0300

    Created showProducts Function

--- src/Products.js
+++ src/Products.js
@@ -1,52 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Products() {
   const [productType, setProductType] = useState("all");
+  const [items, setItems] = useState([]);
 
-  function showProducts() {}
-
-  function searchProduct() {
-    fetch(`https://fakestoreapi.com/products`)
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/categories`)
       .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((json) => setItems(json));
+  }, [productType]);
+
+  function showProducts() {
+    return (
+      <div>
+        <button
+          onClick={() => setProductType("all")}
+          className="btn btn-outline-success me-2 mt-2"
+        >
+          All
+        </button>
+        <button
+          onClick={() => setProductType("men's clothing")}
+          className="btn btn-outline-success me-2 mt-2"
+        >
+          Men's Clothing
+        </button>
+        <button
+          onClick={() => setProductType("women's clothing")}
+          className="btn btn-outline-success me-2 mt-2"
+        >
+          Women's Clothing
+        </button>
+        <button
+          onClick={() => setProductType("jewelery")}
+          className="btn btn-outline-success me-2 mt-2"
+        >
+          Jewelery
+        </button>
+        <button
+          onClick={() => setProductType("electronics")}
+          className="btn btn-outline-success mt-2"
+        >
+          Electronics
+        </button>
+      </div>
+    );
   }
 
   return (
     <div className="Products mb-5">
       <h1 className="mt-5 mb-3">Latest Products</h1>
       <hr />
-      <button
-        onClick={() => setProductType("all")}
-        className="btn btn-outline-success me-2 mt-2"
-      >
-        All
-      </button>
-      <button
-        onClick={() => setProductType("men's clothing")}
-        className="btn btn-outline-success me-2 mt-2"
-      >
-        Men's Clothing
-      </button>
-      <button
-        onClick={() => setProductType("women's clothing")}
-        className="btn btn-outline-success me-2 mt-2"
-      >
-        Women's Clothing
-      </button>
-      <button
-        onClick={() => setProductType("jewelery")}
-        className="btn btn-outline-success me-2 mt-2"
-      >
-        Jewelery
-      </button>
-      <button
-        onClick={() => setProductType("electronics")}
-        className="btn btn-outline-success mt-2"
-      >
-        Electronics
-      </button>
       <h1>{productType}</h1>
-      <div>{searchProduct()}</div>
+      <div>{showProducts()}</div>
     </div>
   );
 }
