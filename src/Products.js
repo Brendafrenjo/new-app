abf1dec529fca762cd4cb61879a736eb590111eb
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-11-22 14:00:30 +0300

    aded filterProduct function and included useState

--- src/Products.js
+++ src/Products.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 
+
 export default function Products() {
-  const [productType, setProductType] = useState([]);
+  const [productType, setProductType] = useState("all");
   const [items, setItems] = useState([]);
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products`)
@@ -10,52 +13,58 @@ export default function Products() {
       .then((json) => setItems(json));
   }, [productType]);
 
+  function filterProduct() {
+    
+  }
+
   function showProducts() {
     return (
       <div>
         <button
-          onClick={() => setProductType("all")}
+          onClick={() => filter(data)}
           className="btn btn-outline-success me-2 mt-2"
         >
           All
         </button>
         <button
-          onClick={() => setProductType("men's clothing")}
+          onClick={() => filterProduct("men's clothing")}
           className="btn btn-outline-success me-2 mt-2"
         >
           Men's Clothing
         </button>
         <button
-          onClick={() => setProductType("women's clothing")}
+          onClick={() => filterProduct("women's clothing")}
           className="btn btn-outline-success me-2 mt-2"
         >
           Women's Clothing
         </button>
         <button
-          onClick={() => setProductType("jewelery")}
+          onClick={() => filterProduct("jewelery")}
           className="btn btn-outline-success me-2 mt-2"
         >
           Jewelery
         </button>
         <button
-          onClick={() => setProductType("electronics")}
+          onClick={() => filterProduct("electronics")}
           className="btn btn-outline-success mt-2"
         >
           Electronics
         </button>
+        <h1>{productType}</h1>
+        {items.map((item) => {
+          return <pre>{JSON.stringify(item)}</pre>;
+        })}
       </div>
     );
   }
 
   return (
     <div className="Products mb-5">
-      <h1 className="mt-5 mb-3">Latest Products</h1>
-      <hr />
-      <div>{showProducts()}</div>
-      <h1>{productType}</h1>
-      {items.map((item) => {
-        return <pre>{JSON.stringify(item)}</pre>;
-      })}
+      <div className="container">
+        <h1 className="mt-5 mb-3">Latest Products</h1>
+        <hr />
+        <div>{showProducts()}</div>
+      </div>
     </div>
   );
 }
